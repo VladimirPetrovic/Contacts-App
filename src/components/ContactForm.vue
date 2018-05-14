980ad14c41567ba4aac5ed3cc996442c57dc4247
Vladimir Petrovic <petrovic.vladimir92@gmail.com>
2018-05-14 22:00:17 +0200

    Email validator

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,21 +1,42 @@
 <template>
   <div>
-    <form class="col-6 mt-2" @submit.prevent="manageContact()">
+    <form class="col-6 mt-2" v-validate="validationRules">
       <div class="form-group">
         <label for="firstName">First Name</label>
-        <input type="text" v-model="contact.first_name" v-focus-on class="form-control" id="firstName">
+        <input
+          type="text"
+          v-model="contact.first_name"
+          name="first_name"
+          v-focus-on
+          class="form-control"
+          id="firstName">
       </div>
       <div class="form-group">
         <label for="lastName">Last Name</label>
-        <input type="text" v-model="contact.last_name" class="form-control" id="lastName">
+        <input
+          type="text"
+          name="last_name"
+          v-model="contact.last_name"
+          class="form-control"
+          id="lastName">
       </div>
       <div class="form-group">
         <label for="email">Email address</label>
-        <input type="email" v-model="contact.email" class="form-control" id="email">
+        <input
+          type="text"
+          name="email"
+          v-model="contact.email"
+          class="form-control"
+          id="email">
       </div>
       <div class="form-group">
         <label for="number">Number</label>
-        <input type="number" v-model="contact.number" class="form-control" id="number">
+        <input
+          name="number"
+          type="number"
+          v-model="contact.number"
+          class="form-control"
+          id="number">
       </div>
       <button type="submit" class="btn btn-primary mr-2">Submit</button>
       <button type="reset" class="btn btn-warning mr-2">Reset</button>
@@ -31,6 +52,13 @@ export default {
         manageContact() {
             this.$emit('manageContact')
         }
+    },
+    data() {
+      return {
+        validationRules: {
+          email: ['required', 'email']
+        }
+      }
     }
 }
 </script>
--- src/plugins/MyDirectives.js
+++ src/plugins/MyDirectives.js
@@ -1,10 +1,62 @@
+const RULES = {
+  REQUIRED: 'required',
+  NUMBER: 'number',
+  EMAIL: 'email'
+}
+
+
+const MESSAGES_CLASS_NAME = 'validator-messages'
+
+const removeMessageErrorElement = (element) => {
+  let oldMessageElement = element.querySelector(`#${MESSAGES_CLASS_NAME}`)
+
+  if (oldMessageElement) {
+    oldMessageElement.remove()
+  }
+}
+
+const addMessageErrorElement = (element, key) => {
+  let messageElement = document.createElement('div')
+  messageElement.id = MESSAGES_CLASS_NAME
+
+  messageElement.innerHTML = `${key.toUpperCase()} is requiered`
+  element.appendChild(messageElement)
+}
+
 const MyDirectives = {
   install: function(Vue) {
     Vue.directive('focusOn', {
-      inserted: function (element) {
+      inserted: function(element) {
         element.focus()
       }
-    });
+    })
+
+    Vue.directive('validate', {
+      inserted: function (element, binding) {
+        let validationRules = binding.value
+
+        element.addEventListener('submit', (event) => {
+          event.preventDefault()
+
+          Object.keys(validationRules).forEach(key => {
+            let input = element.querySelector(`#${key}`)
+
+            if (!input) {
+              throw new Error(
+                `Input element for validation rule ${key} not found!`)
+            }
+
+            console.log()
+            if (validationRules[key].indexOf(RULES.REQUIRED) > -1 && !input.value.length) {
+              removeMessageErrorElement(element)
+              addMessageErrorElement(element, key)
+            } else {
+              removeMessageErrorElement(element)
+            }
+          });
+        })
+      }
+    })
   }
 }
 
